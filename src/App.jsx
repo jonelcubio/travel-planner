b42dbe8f50272a7f7cc58eccf9d5937205f3c814
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-08-12 19:20:14 +0800

    ✈️ Configure routes and route on react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import TargetCursor from "./react-bits/TargetCursor";
 import '@mantine/core/styles.css';
-
+import {Routes, Route} from 'react-router-dom'
 import { MantineProvider } from '@mantine/core';
-import { HeaderSimple } from "./components/HeaderSimple";
-import HeroGrid from "./components/HeroGrid";
+import Home from "./pages/Home";
+import Map from "./pages/Map";
+import Preferences from "./pages/Preferences";
+import TripPlanner from "./pages/TripPlanner";
+import Budget from "./pages/Budget";
+
 
 export default function App () {
   return(
     <>
     <MantineProvider>
-    <TargetCursor 
-        spinDuration={2}
-        hideDefaultCursor={true}
-      />
-
-    <HeaderSimple />
-    <HeroGrid />
-    
+      <TargetCursor 
+          spinDuration={2}
+          hideDefaultCursor={true}
+        />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trip-planner" element={<TripPlanner />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/settings" element={<Preferences />} />
+        <Route path="/github-repo" element={'https://github.com/jonelcubio/travel-planner'} />
+      </Routes>
     </MantineProvider>
     </>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { HeaderSimple } from "../components/HeaderSimple";
+import HeroGrid from "../components/HeroGrid";
+
+export default function Home() {
+  return(
+    <>
+    <HeaderSimple />
+    <HeroGrid />
+    </>
+  );
+}
